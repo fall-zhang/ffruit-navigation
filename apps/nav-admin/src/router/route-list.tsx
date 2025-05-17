0b06aa7e5ca0b82f71bf9f44c4a3bb7f1b1b8a91
fall-zhang <zhangbofei@163.com>
2025-05-17 10:15:56 +0800

    更新路由

---
+++ apps/nav-admin/src/router/route-list.tsx
@@ -0,0 +1,48 @@
+/**
+ * useRoute 创建的路由
+ */
+import React, { FC, ReactNode, Suspense } from 'react'
+import { useRoutes, BrowserRouter, RouteObject, Navigate } from 'react-router-dom'
+
+// import ContactMe from '@/pages/contact/contact'
+// import pageRoutes from './pageRoutes'
+// 这里面使用的是 createRoutesFromChildren createBrowserRouter 生成的路由
+// import type { RouteObject } from 'react-router-dom'
+import LoginPage from '@/pages/user/Login'
+import AuditPage from '@/pages/nav/Audit'
+import ListPage from '@/pages/nav/List'
+import CategoryPage from '@/pages/nav/Category'
+
+export const routeList:RouteObject[] = [
+  {
+    path: '/user',
+    children: [
+      {
+        path: '/user',
+        children: [
+          // name: '登录'
+          { path: '/user/login', element: <LoginPage /> }
+        ]
+      }
+    ]
+  },
+  // name: '审核列表',
+  { path: '/nav/audit', element: <AuditPage/> },
+  //  name: '导航列表',
+  { path: '/nav/list', element: <ListPage /> },
+  // name: '分类列表',
+  { path: '/nav/category', element: <CategoryPage /> },
+  // name: '标签列表',
+  {
+    path: '/nav/tag',
+    async lazy () {
+      const Component = (await import('@/pages/nav/Tag')).default
+      return { Component }
+    }
+  },
+
+  { path: '/', element: <Navigate to="/nav/audit"></Navigate> },
+  { element: './404' }
+
+
+]
---
+++ apps/nav-admin/src/router/router.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react'
+import { Route, Navigate, createBrowserRouter, RouterProvider, BrowserRouter } from 'react-router-dom'
+import { ErrorBoundary } from 'react-error-boundary'
+// 同步方式加载所有路由页面
+import { routeList } from './route-list'
+
+const BaseRoute = createBrowserRouter(routeList)
+export default function () {
+  return <ErrorBoundary fallback={<div>Something went wrong</div>}>
+    <RouterProvider router={BaseRoute}>
+    </RouterProvider>
+  </ErrorBoundary>
+}
