0edfab569fff7c6c48c2f35d382a7de525bf2e98
fall-zhang <zhangbofei@163.com>
2025-05-24 00:24:03 +0800

    移除 footer，更新 login 页面

---
+++ apps/nav-admin/src/components/login-form.tsx
@@ -0,0 +1,80 @@
+import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { FormEvent, FormEventHandler, useRef } from 'react'
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
+
+export function LoginForm ({
+  className,
+  onSubmitForm,
+  ...props
+}: React.ComponentProps<'div'>&{
+onSubmitForm(form:FieldValues):void
+}) {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm()
+  // const formRef = useRef(null)
+  const onSubmit = (form:FieldValues) => {
+    onSubmitForm(form)
+  }
+  return (<div className={cn('flex flex-col gap-6', className)} {...props}>
+    <Card>
+      <CardHeader>
+        {/* <CardTitle>Login to your account</CardTitle> */}
+        <CardTitle className='text-center'>登录</CardTitle>
+        <CardDescription>
+          {/* Enter your email below to login to your account */}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-3">
+              <Label htmlFor="username">账号</Label>
+              <Input
+                placeholder="m@example.com"
+                {...register('username', { required: true })}
+              />
+            </div>
+            <div className="grid gap-3">
+              <div className="flex items-center">
+                <Label htmlFor="password">密码</Label>
+                <a
+                  href="#"
+                  className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                >
+                    忘记密码
+                </a>
+              </div>
+              <Input type="password" {...register('password', { required: true })}/>
+              {errors.exampleRequired && <span className='text-red-600'>请输入密码</span>}
+            </div>
+            <div className="flex flex-col gap-3">
+              <Button type="submit" className="w-full cursor-pointer">
+                登录
+              </Button>
+              {/* <Button variant="outline" className="w-full">
+                  Login with Google
+                </Button> */}
+            </div>
+            <input type="submit" />
+          </div>
+          <div className="mt-4 text-center text-sm">
+              没有账户？{' '}
+            <a href="#" className="underline underline-offset-4">
+                立即注册
+            </a>
+          </div>
+        </form>
+      </CardContent>
+    </Card>
+  </div>
+  )
+}
--- apps/nav-admin/src/pages/user/Login/index.tsx
+++ apps/nav-admin/src/pages/user/Login/index.tsx
@@ -1,21 +1,14 @@
-import {
-  AlipayCircleOutlined,
-  LockOutlined,
-  MobileOutlined,
-  TaobaoCircleOutlined,
-  UserOutlined,
-  WeiboCircleOutlined
-} from '@ant-design/icons'
 import { Alert, Space, message, Tabs } from 'antd'
-import React, { useState } from 'react'
+import React, { FormEvent, FormEventHandler, useState } from 'react'
 import ProForm, { ProFormCaptcha, ProFormCheckbox, ProFormText } from '@ant-design/pro-form'
 // import {Link, history, useModel} from 'umi';
 import { Link, useNavigate } from 'react-router-dom'
-import Footer from '@/components/Footer'
 import styles from './index.module.less'
 import { login } from '@/apis/api'
 import { setPersistenceData } from '@/utils/persistence'
 import { CURRENT_USER, TOKEN } from '@/constants'
+import { LoginForm } from '@/components/login-form'
+import { FieldValues } from 'react-hook-form'
 
 
 const Login: React.FC = () => {
@@ -36,25 +29,24 @@ const Login: React.FC = () => {
   }
 
 
-  const handleSubmit = async (values: any) => {
+  const handleSubmit = async (form:FieldValues) => {
     setSubmitting(true)
 
     try {
       // 登录
-      const res: any = await login({ username: values.username as string, password: values.password as string })
-
+      const res: any = await login({ username: form.username as string, password: form.password as string })
       if (res?.data) {
         const defaultloginSuccessMessage = '登录成功！'
         message.success(defaultloginSuccessMessage)
         setInitialState({
           currentUser: {
-            name: values.username,
+            name: form.username,
             access: 'admin'
           }
         })
         goto()
         setPersistenceData(TOKEN, res.data)
-        setPersistenceData(CURRENT_USER, { name: values.username })
+        setPersistenceData(CURRENT_USER, { name: form.username })
         return
       } // 如果失败去设置用户错误信息
 
@@ -68,18 +60,18 @@ const Login: React.FC = () => {
   }
 
   return (<div className={styles.container}>
-    <div className={styles.content}>
-      <div className={styles.top}>
-        <div className={styles.header}>
-          <Link to="/">
-            <img alt="logo" className={styles.logo} src="/logo-icon.png"/>
-            <span className={styles.title}>鲜果导航</span>
-          </Link>
-        </div>
-        <div className={styles.desc}>{'鲜果导航，专注独特资源导航'}</div>
+    <div className="flex flex-col min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className='flex flex-col items-center'>
+        <Link to="/">
+          <span className="text-neutral-200 font-bold text-3xl text-center font-size: 33px;">鲜果导航</span>
+        </Link>
+        <div className='mt-3 mb-10 text-neutral-200 text-base'>{'鲜果导航，专注独特资源导航'}</div>
       </div>
-
-      <div className={styles.main}>
+      <div className="w-full max-w-sm pb-16">
+        <LoginForm onSubmitForm={handleSubmit}/>
+      </div>
+    </div>
+    {/* <div className={styles.main}>
         <ProForm
           initialValues={{
             autoLogin: true
@@ -130,9 +122,8 @@ const Login: React.FC = () => {
             />
           </>
         </ProForm>
-      </div>
-    </div>
-    <Footer/>
+      </div> */}
+    {/* <Footer/> */}
   </div>
   )
 }
