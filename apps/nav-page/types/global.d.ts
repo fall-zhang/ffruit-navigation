f66090f1c232c79e8146e436f4ecb00360306cec
fall-zhang <zhangbofei@163.com>
2025-07-13 20:25:02 +0800

    更新类型和配置

---
+++ apps/nav-page/types/global.d.ts
@@ -0,0 +1,19 @@
+export type LinkItem = {
+  id:string
+  logo:string
+  // 名称
+  name:string
+  // 链接地址
+  href:string
+  view:number
+  star:number
+  createTime:string
+  // 描述
+  desc:string
+  // 创建用户主页
+  creatorUrl:string
+  // 创建用户
+  creator:string
+  //
+  tags:string[]
+}
