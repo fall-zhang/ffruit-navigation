77fed0ac45ff4a5e7b9798b57c01df4ba2bdb1a3
fall-zhang <zhangbofei@163.com>
2025-08-08 22:42:47 +0800

    feat(nav-page): 🚀 更新图片素材

--- apps/nav-page/server/routes/api-hot/[...slug].ts
+++ apps/nav-page/server/routes/api-hot/[...slug].ts
@@ -1,5 +1,6 @@
 import type { HotApiResType } from 'nav-types'
 
+
 export default defineEventHandler(async (event):Promise<HotApiResType> => {
   const slug = event.context.params?.slug || 'bilibili'
   const res = await $fetch('https://api-hot.imsyy.top/' + slug, {
