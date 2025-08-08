36ffbd06694e0775c4e7066e96d5c8e5b8678340
fall-zhang <zhangbofei@163.com>
2025-08-08 13:24:43 +0800

    feat(nav-admin): 🚀 更新 admin 的类型引入

--- apps/nav-admin/src/main.tsx
+++ apps/nav-admin/src/main.tsx
@@ -1,13 +1,15 @@
-import { FC, ReactNode, StrictMode } from 'react'
+import { FC, ReactNode, StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import RouterPage from './router/router'
 import { useBrowserTheme } from './hooks/use-theme'
-// import { useTheme } from 'next-themes'
 
+// import { useTheme } from 'next-themes'
+type inferType<T> = T extends (x: infer P) => unknown ? P : T
 const ThemeProvide:FC<{
   children:ReactNode
 }> = ({ children }) => {
+  const [state, setState] = useState('')
   const theme = useBrowserTheme()
   return <>
     {children}
--- apps/nav-admin/src/pages/nav-list/data-schema.ts
+++ apps/nav-admin/src/pages/nav-list/data-schema.ts
@@ -1,26 +1,5 @@
-import z from 'zod'
+import { navDataSchema } from 'nav-types'
+import type { NavDataType } from 'nav-types'
+export const tableDataSchema = navDataSchema
 
-export const tableDataSchema = z.object({
-  id: z.string().nullable(),
-  name: z.string().min(2, '名称长度至少为 2'),
-  href: z.string(),
-  desc: z.string(),
-  logo: z.string(),
-  authorName: z.string(),
-  authorUrl: z.string(),
-  auditTime: z.string(),
-  createTime: z.string(),
-  tag: z.array(z.object({
-    id: z.string(),
-    name: z.string()
-  })),
-  view: z.number(),
-  star: z.number(),
-  // 审核状态 1 审核中 2 拒绝 3 通过
-  status: z.string(), // 'CHECK" | "REJECT" | "PASS'
-  // 访问状态 1 正常访问 2 需要代理 3 网站已停用
-  accessState: z.number() // "NORMAL" | "PROXY" | "DEACTIVATE"
-})
-
-
-export type TableItemType = z.infer<typeof tableDataSchema>
+export type navDataSchema = NavDataType
