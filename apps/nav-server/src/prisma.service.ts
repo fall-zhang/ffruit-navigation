889a55ac130075cbad4752dd95cded55098169db
fall-zhang <zhangbofei@163.com>
2025-10-05 13:26:16 +0800

    feat(nav-server): 🚀 更新 Prisma 生成

--- apps/nav-server/src/prisma.service.ts
+++ apps/nav-server/src/prisma.service.ts
@@ -1,7 +1,16 @@
 import { Injectable, OnModuleInit } from '@nestjs/common'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@/generated/prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+
+const connectionString = `${process.env.DATABASE_URL}`
+
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    const adapter = new PrismaPg({ connectionString })
+    super({ adapter })
+  }
+
   // optional  如果没有连接，prisma 会在第一次调用数据库时连接
   async onModuleInit () {
     await this.$connect()
